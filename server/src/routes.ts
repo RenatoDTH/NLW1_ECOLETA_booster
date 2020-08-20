5116e539ba5a92f8ab6dcf6b4e8c1710d428b9b3
Renato Castro <renatocastro02@hotmail.com>
2020-08-20 14:01:20 -0300

    Routes do server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController'
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({message: 'hello world'})
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return { 
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+         }
+    })
+
+    return res.json(serializedItems);
  });
+
+ routes.post('/points', PointsController.create );
  
 export default routes;
