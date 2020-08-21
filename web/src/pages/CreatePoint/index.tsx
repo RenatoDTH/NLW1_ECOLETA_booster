585a431e6bff4e70826e2ccc4fa87edc5e25c47f
Renato Castro <renatocastro02@hotmail.com>
2020-08-21 16:26:09 -0300

    início da web

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const CreatePoint: React.FC = () => {
+  return (
+      <h1>Hello Planet</h1>
+  );
+}
+
+export default CreatePoint;
