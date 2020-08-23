108881be70dbbb2e9ad8cc50dda640fadccf3364
Renato Castro <renatocastro02@hotmail.com>
2020-08-23 15:55:21 -0300

    Inicio do mobile + ajuste no localhost

---
+++ mobile/routes.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './src/pages/Home';
+import Points from './src/pages/Points';
+import Detail from './src/pages/Detail';
+
+const appStack = createStackNavigator();
+
+const Routes = () => (
+  <NavigationContainer>
+    <appStack.Navigator
+      headerMode="none"
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: '#f0f0f5',
+        },
+      }}
+    >
+      <appStack.Screen name="Home" component={Home} />
+      <appStack.Screen name="Points" component={Points} />
+      <appStack.Screen name="Detail" component={Detail} />
+    </appStack.Navigator>
+  </NavigationContainer>
+);
+
+export default Routes;
